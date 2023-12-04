3b7dbe5198ac236e8f672602a209240a4f8aad9f
Daniel Temple <dantdj@gmail.com>
2023-12-04 19:46:13 +0000

    Minor cleanup for day 4 - Some small readability and performance enhancements
    Replace some of the string splitting with a regex. This has a small performance impact, but results in a more readable input parsing.
    
    Pre-compute the matches for the cards, instead of repeatedly re-calculating the winning numbers for the card. This gives a decent performance boost - 33% on my machine

--- 2023/4/main.go
+++ 2023/4/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -46,9 +47,21 @@ func partOne() {
 func partTwo() {
 	input, _ := readInput("real-input")
 
-	// Populate initial card stacks
+	// Populate initial card stacks, and calculate the win states so we know how many new cards
+	// to create if they win. This saves time on calculating the answer every loop, as it'll always remain
+	// the same
 	cardStacks := []CardStack{}
 	for _, card := range input {
+		matches := 0
+		for _, myNumber := range card.MyNumbers {
+			for _, winningNumber := range card.WinningNumbers {
+				if myNumber == winningNumber {
+					matches++
+				}
+			}
+		}
+
+		card.NumberOfMatches = matches
 		cardStacks = append(cardStacks, CardStack{CardNumber: card.CardNumber, Cards: []ScratchCard{card}})
 	}
 
@@ -57,17 +70,11 @@ func partTwo() {
 	for _, cardStack := range cardStacks {
 		for _, card := range cardStack.Cards {
 			numberOfCards += 1
-			matches := 0
-			for _, myNumber := range card.MyNumbers {
-				for _, winningNumber := range card.WinningNumbers {
-					if myNumber == winningNumber {
-						matches++
-					}
-				}
-			}
 			
-			for i := card.CardNumber; i < matches+card.CardNumber; i++ {
-				cardStacks[i].Cards = append(cardStacks[i].Cards, input[i])
+			for i := card.CardNumber; i < card.NumberOfMatches+card.CardNumber; i++ {
+				// Add a new copy of the original card on the particular stack - this ensures
+				// that the pre-computed match count is persisted
+				cardStacks[i].Cards = append(cardStacks[i].Cards, cardStacks[i].Cards[0])
 			} 
 		}
 	}
@@ -86,37 +93,31 @@ func readInput(filename string) ([]ScratchCard, error) {
 	scratchcards := []ScratchCard{}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
+		findNumRegex := `\d+`
+		re := regexp.MustCompile(findNumRegex)
+
 		// Divide into card number and card state
 		cardSplit := strings.Split(scanner.Text(), ": ")
-		cardNumberStrs := strings.Split(cardSplit[0], " ")
-		cardNumber, _ := strconv.Atoi(cardNumberStrs[len(cardNumberStrs) - 1])
+
+		// Find card number
+		match := re.FindString(cardSplit[0])
+		cardNumber, _ := strconv.Atoi(match)
 
 		// Split into my numbers and winning numbers
 		numberSplit := strings.Split(cardSplit[1], " | ")
 
-		// Split into individual values
-		myNumbers := strings.Split(numberSplit[0], " ")
-		winningNumbers := strings.Split(numberSplit[1], " ")
+		myNumbers := re.FindAllString(numberSplit[0], -1)
+		winningNumbers := re.FindAllString(numberSplit[1], -1)
 
 		scratchcard := ScratchCard{CardNumber: cardNumber}
 
 		for _, numberStr := range myNumbers {
 			value, _ := strconv.Atoi(numberStr)
-			if value == 0 {
-				// We probably tried to parse an empty string, skip
-				// This can be left over from the string split
-				continue
-			}
 			scratchcard.MyNumbers = append(scratchcard.MyNumbers, value)
 		}
 
 		for _, numberStr := range winningNumbers {
 			value, _ := strconv.Atoi(numberStr)
-			if value == 0 {
-				// We probably tried to parse an empty string, skip
-				// This can be left over from the string split
-				continue
-			}
 			scratchcard.WinningNumbers = append(scratchcard.WinningNumbers, value)
 		}
 
@@ -130,6 +131,7 @@ type ScratchCard struct {
 	CardNumber     int
 	MyNumbers      []int
 	WinningNumbers []int
+	NumberOfMatches int
 }
 
 type CardStack struct {
